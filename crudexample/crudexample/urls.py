41e1c6d0e46669ec013b913443375eea3b832851
hr4official <hardikrthod3695@gmail.com>
2019-12-31 17:26:35 +0530

    h

--- crudexample/crudexample/urls.py
+++ crudexample/crudexample/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from employee import views  
 
 urlpatterns = [
+    #url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
+    #url(r'^admin/', include(admin.site.urls)),
+    path('jet/',admin.site.urls),
+    path('jet/dashboard/',include('jet.dashboard.urls', 'jet-dashboard')),
+  #  urls('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     path('admin/', admin.site.urls),
     path('admin/', admin.site.urls),  
     path('emp', views.emp),  
